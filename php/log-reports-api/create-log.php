820e1fc95263d45cae4992eb92aa791d91194f26
Tudor Gulin <gulintudor@gmail.com>
2024-05-23 18:08:48 +0300

    Update create-log.php

--- php/log-reports-api/create-log.php
+++ php/log-reports-api/create-log.php
@@ -1,13 +1,13 @@
 <?php
 
+include './log-functions.php';
+
   error_reporting(0);
 
   header('Access-Control-Allow-Origin: *');
   header('Content-Type: application/json');
   header('Access-Control-Allow-Methods: POST');
   header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
-
-  include './log-functions.php';
 
   session_start();
   $user_id = $_SESSION['user_id'];
